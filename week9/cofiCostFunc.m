a4e3992f8335061da41e108b4c0c4e26a0971200
Feng Wei <fw1036994377@outlook.com>
2016-06-13 15:18:43 +0800

    recommend system

--- week9/cofiCostFunc.m
+++ week9/cofiCostFunc.m
@@ -17,6 +17,9 @@ J = 0;
 X_grad = zeros(size(X));
 Theta_grad = zeros(size(Theta));
 
+J = 1/2 * sum(sum( ( (X * Theta' - Y).*R ).^2 )) + lambda/2*(sum(sum(Theta.^2))+sum(sum(X.^2)));
+X_grad = (X * Theta' - Y).*R * Theta + lambda* X;
+Theta_grad = (X'*( (X * Theta' - Y).*R ))' + lambda * Theta;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost function and gradient for collaborative
 %               filtering. Concretely, you should first implement the cost
